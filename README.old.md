f932a0b52ed72a043c1231c74a8ff1bcef50434c
Your Name <you@example.com>
2020-11-01 07:14:32 -0800

    Initial npm files

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# store
+A full fledged , higly functional and resposive ReactJS webpage on online e-commerce website. 
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Just checking if this works
+    </div>
+  );
+}
+
+export default App;
